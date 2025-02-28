aeca582cf852793662180a0c151edb5ebacb9752
Leandro Pereira <leandronobper@gmail.com>
2025-02-28 13:35:41 -0300

    Add initial gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [icon()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+  output: 'server',
+  adapter: vercel(),
 });
---
+++ src/pages/api/pexels.ts
@@ -0,0 +1,24 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+  const API_KEY = import.meta.env.PEXELS_API_KEY;
+  const DEFAULT_PAGE = 1;
+  const DEFAULT_PER_PAGE = 20;
+
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page") || DEFAULT_PAGE;
+  const per_page = url.searchParams.get("per_page") || DEFAULT_PER_PAGE;
+
+  const apiUrl = `https://api.pexels.com/v1/curated?page=${page}&per_page=${per_page}`;
+
+  const response = await fetch(apiUrl, {
+    headers: {
+      Authorization: API_KEY,
+    },
+  });
+
+  const data = await response.json();
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
